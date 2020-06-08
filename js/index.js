7bbbe80b3b6ba7b1bec557a042b01f8e4d63fee1
FishC_Service <fishc_service@126.com>
2020-06-08 22:46:56 +0800

    完成首页交互和个人中心部分布局

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-$(function(){
+$(function () {
 
+    //固定距离滑出返回顶部按钮
+    $(window).scroll(function () {
+        1e3 < $("html,body").scrollTop() ? $(".retop").slideDown() : $(".retop").slideUp()
+    })
+    $(".retop").click(function () {
+        var l = setInterval(function () {
+            var o = $("html,body").scrollTop();
+            $("html,body").scrollTop(o - 50), o <= 0 && clearInterval(l)
+        }, 30)
+    })
+
+    //点击返回顶部
+    $('.retop').click(function(){
+        $('html,body').animate({scrollTop:0},300)
+        return false
+    })
 })
