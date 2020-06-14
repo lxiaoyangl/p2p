5b29c06a40b00beab99d6b7e06074d5a27790c1d
FishC_Service <fishc_service@126.com>
2020-06-14 21:34:03 +0800

    完成所有接口及页面的搭建和表单验证,交互

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ $(function () {
     $(window).scroll(function () {
         1e3 < $("html,body").scrollTop() ? $(".retop").slideDown() : $(".retop").slideUp()
     })
+
     $(".retop").click(function () {
         var l = setInterval(function () {
             var o = $("html,body").scrollTop();
@@ -29,24 +30,24 @@ $(function () {
             case "":
                 $('#main').load("../../pages/home.html");
                 break;
-                //首页
+            //首页
             case "#home":
                 $('#main').load("../../pages/home.html");
                 break;
-                //个人中心
+            //个人中心
             case "#mymsg":
                 /* $('#main').load("../../pages/mymsg.html");
                 $('#rightContent').load('../../pages/mymsg/getuserinfo.html') */
                 loadPage('#mymsg/getuserinfo')
                 break;
-                //个人中心==>账户信息
+            //个人中心==>账户信息
             case "#mymsg/getuserinfo":
                 loadPage(hash)
                 break;
-                //个人中心==>更新资料
+            //个人中心==>更新资料
             case "#mymsg/updateuser":
                 loadPage(hash)
-                //个人中心==>发布贷款
+            //个人中心==>发布贷款
             case "#mymsg/borrow_apply":
                 loadPage(hash)
                 break;
@@ -58,44 +59,44 @@ $(function () {
             case "#invest":
                 $('#main').load("../../pages/invest.html");
                 break;
-                //404
+            //404
             default:
                 $('#main').load("../../pages/404.html");
         }
         chagenNav(hash);
     }
     //加载个人中心二级页面
-    function loadPage(hash){
+    function loadPage(hash) {
         // 去掉获取到的hash的#号
-        hash = hash.substr(1) 
+        hash = hash.substr(1)
         // 切换页面
-        if($('#rightContent').length){//点击
-            $('#rightContent').load('../../pages/'+ hash +'.html')
+        if ($('#rightContent').length) {//点击
+            $('#rightContent').load('../../pages/' + hash + '.html')
             activePage(hash)
-        }else{//刷新
-            $('#main').load('../../pages/mymsg.html',function(){
-            $('#rightContent').load('../../pages/'+ hash +'.html')
-            activePage(hash)
+        } else {//刷新
+            $('#main').load('../../pages/mymsg.html', function () {
+                $('#rightContent').load('../../pages/' + hash + '.html')
+                activePage(hash)
             })
         }
     }
 
     //二级路由激活样式改变
-    function activePage(hash){
+    function activePage(hash) {
         //此时的hash为去掉#的值
-        
+
         $('.list-group li a').removeClass('active')
-        $('.list-group li a[href="#'+hash+'"]').addClass('active')
+        $('.list-group li a[href="#' + hash + '"]').addClass('active')
     }
 
     //通过hash添加导航栏激活样式
     function chagenNav(hash) {
         //当hash为空时就将hash变量改为#home
-        if(hash === '') hash = '#home'
+        if (hash === '') hash = '#home'
         //只要hash包含mymsg就将hash变量改为#mymsg
-        if(hash.includes('mymsg')) hash = '#mymsg'
+        if (hash.includes('mymsg')) hash = '#mymsg'
         //添加激活样式
-        $('.second .list .nav-item[href="'+ hash +'"]').addClass('active')
+        $('.second .list .nav-item[href="' + hash + '"]').addClass('active')
             .closest('li').siblings('li').find('a').removeClass('active')
         /*  
         $('.second .list .nav-item').removeClass('active')
@@ -109,7 +110,7 @@ $(function () {
         //获取本地存储数据
         var username = localStorage.getItem('username')
         var pwd = localStorage.getItem('uid')
-        if (pwd && username) {            
+        if (pwd && username) {
             //将首页的内容改变
             $('#login').html('<a href="#">' + username + '</a> / <a href="#" id="regOut">注销</a>')
         } else {
@@ -124,6 +125,4 @@ $(function () {
             $('#login').html('<a href="./login.html">登录</a> / <a href="#">注册</a>')
         }
     })
-
-    
 })
